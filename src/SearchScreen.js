c45f3da8913ad032d45798923a22740073f8ad41
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-12-08 14:44:17 +0200

    Assemble navigator

---
+++ src/SearchScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>Search Screen</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
